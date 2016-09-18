f135993562c712b8ac17446353869939dd767a7c
Peter <petercblanco@gmail.com>
2016-09-18 11:13:29 -0400

    oneVsAll file complete

--- ml/machine-learning-ex3/ex3/oneVsAll.m
+++ ml/machine-learning-ex3/ex3/oneVsAll.m
@@ -49,10 +49,15 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Loop through each number
 
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+initial_theta = zeros(n + 1, 1); 
 
-
-
+for c = 1 : num_labels;
+    [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+    all_theta(c, :) = theta';
+endfor
 
 
 
