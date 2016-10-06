de54914729d7c21ce5600c0d7edad4276cf6d25c
Peter <petercblanco@gmail.com>
2016-10-06 00:37:33 -0400

    ml ex6 complete

--- ml/machine-learning-ex6/ex6/dataset3Params.m
+++ ml/machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+% C = 1;
+% sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+step_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+min_err = realmax();
 
-
-
-
-
+for i = 1:length(step_vec)
+    for j = 1:length(step_vec)
+        c_step = step_vec(i);
+        sigma_step = step_vec(j);
+        model = svmTrain(X, y, c_step, @(x1, x2) gaussianKernel(x1, x2, sigma_step)); 
+        p = svmPredict(model, Xval);
+        err = mean(double(p ~= yval));
+        if err < min_err
+            C = c_step;
+            sigma = sigma_step;
+            min_err = err;
+    end 
+end
 
 % =========================================================================
 
